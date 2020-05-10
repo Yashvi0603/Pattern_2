f3e560b4c995e736a9f05b0769520e8041af1af6
Yashvi0603 <65069399+Yashvi0603@users.noreply.github.com>
2020-05-10 18:52:12 +0530

    Add files via upload

---
+++ patterns2.c
@@ -0,0 +1,28 @@
+/*
+	Name: C Program for Print Pattern
+	INPUT = 5
+
+	1
+	12
+	123
+	1234
+	12345
+*/
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,n;
+	printf("Enter Number : "); 
+	scanf("%d",&n);
+	
+	for(i=0;i<=n+1;i++)
+	{
+		for(j=1;j<i;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
